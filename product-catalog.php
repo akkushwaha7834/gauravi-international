718818633487e15e4115800e3cc4b1bcfd8b726f
akkushwaha7834 <akkushwaha018@gmail.com>
2024-12-17 17:19:36 +0530

    Product section comntent changed

--- product-catalog.php
+++ product-catalog.php
@@ -33,22 +33,13 @@
                         <!-- Nav Tabs List -->
                         <ul class="nav nav-tabs d-none d-lg-block" id="myTab" role="tablist">
                            <li class="nav-item" role="presentation">
-                              <button class="nav-link  active" id="#Forex-tab" data-bs-toggle="tab" data-bs-target="#Forex" type="button" role="tab" aria-controls="Forex" aria-selected="true">24/5 Trading <i class="ri-arrow-right-line"></i></button>
+                              <button class="nav-link  active" id="#Forex-tab" data-bs-toggle="tab" data-bs-target="#Forex" type="button" role="tab" aria-controls="Forex" aria-selected="true">Luxury Bedding Collections <i class="ri-arrow-right-line"></i></button>
                            </li>
                            <li class="nav-item" role="presentation">
-                              <button class="nav-link " id="Commodities-tab" data-bs-toggle="tab" data-bs-target="#Commodities" type="button" role="tab" aria-controls="Commodities" aria-selected="false" tabindex="-1">Transparent Costs<i class="ri-arrow-right-line"></i></button>
+                              <button class="nav-link " id="Commodities-tab" data-bs-toggle="tab" data-bs-target="#Commodities" type="button" role="tab" aria-controls="Commodities" aria-selected="false" tabindex="-1">Handloom Treasures<i class="ri-arrow-right-line"></i></button>
                            </li>
                            <li class="nav-item" role="presentation">
-                              <button class="nav-link " id="Indices-tab" data-bs-toggle="tab" data-bs-target="#Indices" type="button" role="tab" aria-controls="Indices" aria-selected="false" tabindex="-1">Versatile Market Opportunities<i class="ri-arrow-right-line"></i></button>
-                           </li>
-                           <li class="nav-item" role="presentation">
-                              <button class="nav-link " id="Crypto-tab" data-bs-toggle="tab" data-bs-target="#Crypto" type="button" role="tab" aria-controls="Crypto" aria-selected="false" tabindex="-1">Trade more with less<i class="ri-arrow-right-line"></i></button>
-                           </li>
-                           <li class="nav-item" role="presentation">
-                              <button class="nav-link " id="Stocks-tab" data-bs-toggle="tab" data-bs-target="#Stocks" type="button" role="tab" aria-controls="Stocks" aria-selected="false" tabindex="-1">Unparalleled Liquidity <i class="ri-arrow-right-line"></i></button>
-                           </li>
-                           <li class="nav-item" role="presentation">
-                              <button class="nav-link " id="Diverse-tab" data-bs-toggle="tab" data-bs-target="#Diverse" type="button" role="tab" aria-controls="Diverse" aria-selected="false" tabindex="-1">Diverse Market Access <i class="ri-arrow-right-line"></i></button>
+                              <button class="nav-link " id="Indices-tab" data-bs-toggle="tab" data-bs-target="#Indices" type="button" role="tab" aria-controls="Indices" aria-selected="false" tabindex="-1">Specialized B2B Offerings<i class="ri-arrow-right-line"></i></button>
                            </li>
                         </ul>
                      </div>
@@ -94,43 +85,6 @@
                               </div>
                            </div>
                         </div>
-                        <div class="tab-pane accordion-item fade" id="Crypto" role="tabpanel" aria-labelledby="Crypto-tab">
-                           <h2 class="accordion-header  mobile-btn d-lg-none" id="headingCrypto">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseCrypto" aria-expanded="false" aria-controls="collapseCrypto">Trade more with less </button>
-                           </h2>
-                           <div id="collapseCrypto" class="accordion-collapse collapse d-lg-block" aria-labelledby="headingCrypto" data-bs-parent="#myTabContent">
-                              <div class="accordion-body">
-                                 <h3 class="tab-content-title">Trade more with less</h3>
-                                 <p>Trade more with less in Forex, thanks to leverage, enabling you to enter the market with a fraction of the typical capital required. While leverage can boost gains, it's crucial to understand the risks associated with margin trading, as it may also amplify losses.</p>
-                                 <img src="https://www.nxgmarkets.com/images/bgg.png" alt="" class="w-100">
-                              </div>
-                           </div>
-                        </div>
-                        <div class="tab-pane accordion-item fade" id="Stocks" role="tabpanel" aria-labelledby="Stocks-tab">
-                           <h2 class="accordion-header  mobile-btn d-lg-none" id="headingStocks">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseStocks" aria-expanded="false" aria-controls="collapseStocks">Stocks </button>
-                           </h2>
-                           <div id="collapseStocks" class="accordion-collapse collapse d-lg-block" aria-labelledby="headingStocks" data-bs-parent="#myTabContent">
-                              <div class="accordion-body">
-                                 <h3 class="tab-content-title">Unparalleled Liquidity</h3>
-                                 <p>Forex, with a daily turnover of $6.6 trillion,* stands as the world's most liquid market. This liquidity facilitates prompt responses to currency fluctuations, 24 hours a day, 5 days a week, leading to more actionable prices compared to other financial markets.</p>
-                                 <img src="https://www.nxgmarkets.com/images/bgg.png" alt="" class="w-100">
-                              </div>
-                           </div>
-                        </div>
-                        <div class="tab-pane accordion-item fade" id="Diverse" role="tabpanel" aria-labelledby="Diverse-tab">
-                           <h2 class="accordion-header  mobile-btn d-lg-none" id="headingDiverse">
-                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDiverse" aria-expanded="false" aria-controls="collapseDiverse">Diverse Market Access </button>
-                           </h2>
-                           <div id="collapseDiverse" class="accordion-collapse collapse d-lg-block" aria-labelledby="headingDiverse" data-bs-parent="#myTabContent">
-                              <div class="accordion-body">
-                                 <h3 class="tab-content-title">Diverse Market Accessy</h3>
-                                 <p>Forex trading provides exposure to a wide range of global markets. While major currencies dominate, opportunities extend to emerging markets like the Mexican Peso (MXN) and Polish Zloty (PLN).</p>
-                                 <img src="https://www.nxgmarkets.com/images/bgg.png" alt="" class="w-100">
-                              </div>
-                           </div>
-                        </div>
-
                      </div>
                   </div>
                </div>
