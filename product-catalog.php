78be5f914e86103d3d6a6c51659b89d14a0f7aca
Amit Kushwaha <akkushwaha018@gmail.com>
2024-12-28 13:20:47 +0530

    Product page design added

--- product-catalog.php
+++ product-catalog.php
@@ -36,5 +36,103 @@
     </div>
 </section>
 
+<section class="pt-8" style="background: var(--theme-color);">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="text-center">
+                    Bedsheet Collections
+                </h2>
+            </div>
+        </div>
+        <div class="row mt-5 align-items-center mt-4">
+            <div class="col-lg-8 wow fadeInLeft animated animated animated" data-wow-delay="00ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInLeft;">
+                <img src="assets/images/luxury-bedding-collections.jpg" class="img-fluid rounded position-relative z-0">
+            </div>
+            <div class="col-lg-4">
+                <div class="pdt-info position-relative">
+                    <h4>Luxury Cotton Bedsheets</h4>
+                    <p>
+                        Our premium cotton bedsheets offer unparalleled comfort and durability, perfect for high-end retail and luxury hospitality sectors.
+                    </p>
+                    <h5>
+                        Subcategories:
+                    </h5>
+                    <ul class="pdt-category">
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Egyptian Cotton Sheets (400-1000 Thread Count)
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Supima Cotton Bedding
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Organic Cotton Linens
+                        </li>
+                    </ul>
+                    <h5 class="mt-3">Minimum Order Quantity (MOQ): 1,000 sets</h5>
+
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5 align-items-center mt-4">
+            <div class="col-lg-4">
+                <div class="pdt-infoo position-relative">
+                    <h4>Blended Fabric Bedsheets</h4>
+                    <p>
+                        Ideal for partners seeking a balance of comfort, durability, and cost-effectiveness.
+                    </p>
+                    <h5>
+                        Subcategories:
+                    </h5>
+                    <ul class="pdt-category">
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Cotton-Polyester Blend (60/40, 80/20 ratios)
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Bamboo-Cotton Fusion
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Tencel Blend Sheets
+                        </li>
+                    </ul>
+                    <h5 class="mt-3">Minimum Order Quantity (MOQ): 2,000 sets</h5>
+                </div>
+            </div>
+            <div class="col-lg-8 wow fadeInRight animated animated animated" data-wow-delay="00ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInRight;">
+                <img src="assets/images/handloom-treasures.jpg" class="img-fluid rounded">
+            </div>
+        </div>
+        <div class="row mt-5 align-items-center mt-4">
+            <div class="col-lg-8 wow fadeInLeft animated animated animated" data-wow-delay="00ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInLeft;">
+                <img src="assets/images/specialized-b-b-offerings.jpg" class="img-fluid rounded position-relative z-0">
+            </div>
+            <div class="col-lg-4">
+                <div class="pdt-info position-relative">
+                    <h4>Specialty Bedding</h4>
+                    <p>
+                        Catering to specific market needs and trends in the bedding industry.
+                    </p>
+                    <h5>
+                        Subcategories:
+                    </h5>
+                    <ul class="pdt-category">
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Hypoallergenic Microfiber Sheets
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Temperature Regulating Bedding
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Water-Resistant Hotel Linens
+                        </li>
+                    </ul>
+                    <h5 class="mt-3">
+                        Minimum Order Quantity (MOQ): 1,500 sets
+                    </h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php include ('include/footer.php'); ?>
