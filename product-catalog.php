7e270ecab03af40b1630cdd86fc8c21652ffbe52
Amit Kushwaha <akkushwaha018@gmail.com>
2024-12-30 11:13:54 +0530

    Product page done

--- product-catalog.php
+++ product-catalog.php
@@ -1,4 +1,4 @@
-<?php include ('include/header.php'); ?>
+<?php include('include/header.php'); ?>
 
 <!-- Breadcrumb section -->
 <section class="section-space bg-image breadcrumb-overlay position-relative"
@@ -29,7 +29,8 @@
                     Gauravi International: Your Source for Premium Textiles
                 </h2>
                 <p>
-                    Welcome to our comprehensive B2B product catalog. As a leading exporter, we offer an extensive range of high-quality bedsheets and handloom products tailored for global wholesale partners.
+                    Welcome to our comprehensive B2B product catalog. As a leading exporter, we offer an extensive range
+                    of high-quality bedsheets and handloom products tailored for global wholesale partners.
                 </p>
             </div>
         </div>
@@ -45,15 +46,17 @@
                 </h2>
             </div>
         </div>
-        <div class="row mt-5 align-items-center mt-4">
-            <div class="col-lg-8 wow fadeInLeft animated animated animated" data-wow-delay="00ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInLeft;">
+        <div class="row g-4 mt-5 align-items-center mt-4">
+            <div class="col-lg-8 wow fadeInLeft animated animated animated" data-wow-delay="00ms"
+                data-wow-duration="1500ms">
                 <img src="assets/images/luxury-bedding-collections.jpg" class="img-fluid rounded position-relative z-0">
             </div>
             <div class="col-lg-4">
                 <div class="pdt-info position-relative">
                     <h4>Luxury Cotton Bedsheets</h4>
                     <p>
-                        Our premium cotton bedsheets offer unparalleled comfort and durability, perfect for high-end retail and luxury hospitality sectors.
+                        Our premium cotton bedsheets offer unparalleled comfort and durability, perfect for high-end
+                        retail and luxury hospitality sectors.
                     </p>
                     <h5>
                         Subcategories:
@@ -74,8 +77,8 @@
                 </div>
             </div>
         </div>
-        <div class="row mt-5 align-items-center mt-4">
-            <div class="col-lg-4">
+        <div class="row mt-5 g-4 align-items-center mt-4">
+            <div class="col-lg-4 order-lg-1 order-2">
                 <div class="pdt-infoo position-relative">
                     <h4>Blended Fabric Bedsheets</h4>
                     <p>
@@ -98,12 +101,14 @@
                     <h5 class="mt-3">Minimum Order Quantity (MOQ): 2,000 sets</h5>
                 </div>
             </div>
-            <div class="col-lg-8 wow fadeInRight animated animated animated" data-wow-delay="00ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInRight;">
+            <div class="col-lg-8 order-lg-2 order-1 wow fadeInRight animated animated animated" data-wow-delay="00ms"
+                data-wow-duration="1500ms">
                 <img src="assets/images/handloom-treasures.jpg" class="img-fluid rounded">
             </div>
         </div>
-        <div class="row mt-5 align-items-center mt-4">
-            <div class="col-lg-8 wow fadeInLeft animated animated animated" data-wow-delay="00ms" data-wow-duration="1500ms" style="visibility: visible; animation-duration: 1500ms; animation-delay: 0ms; animation-name: fadeInLeft;">
+        <div class="row g-4 mt-5 align-items-center mt-4">
+            <div class="col-lg-8 wow fadeInLeft animated animated animated" data-wow-delay="00ms"
+                data-wow-duration="1500ms">
                 <img src="assets/images/specialized-b-b-offerings.jpg" class="img-fluid rounded position-relative z-0">
             </div>
             <div class="col-lg-4">
@@ -132,7 +137,146 @@
                 </div>
             </div>
         </div>
+        <div class="row  g-4 mt-5 align-items-center mt-4">
+            <div class="col-lg-4">
+                <div class="pdt-infoo position-relative">
+                    <h4>Bulk Fabric Options</h4>
+                    <p>
+                        For partners requiring large quantities of fabric for their own manufacturing.Categories
+                    </p>
+                    <h5>
+                        Subcategories:
+                    </h5>
+                    <ul class="pdt-category">
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Plain Woven Fabrics
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Printed Textiles
+                        </li>
+                        <li>
+                            <i class="ri-arrow-right-double-line"></i> Dyed Fabrics
+                        </li>
+                    </ul>
+                    <h5 class="mt-3">Minimum Order Quantity (MOQ): 5,000 meters per type</h5>
+                </div>
+            </div>
+            <div class="col-lg-8 wow fadeInRight animated animated animated" data-wow-delay="00ms">
+                <img src="assets/images/febric.jpg" class="img-fluid rounded">
+            </div>
+        </div>
     </div>
 </section>
+<section class="section-space">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <h2 class="mb-4 text-center">How to Order</h2>
+            </div>
+        </div>
 
-<?php include ('include/footer.php'); ?>
+        <div class="row g-4">
+            <div class="col-md-6  col-lg-3">
+                <div class="steps-order">
+                    <h3>Step 1</h3>
+                    <p class="mb-0">Browse our catalog and select products of interest</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+
+                <div class="steps-order">
+                    <h3>Step 2</h3>
+                    <p class="mb-0">Contact our B2B sales team for current pricing and availability</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="steps-order">
+                    <h3>Step 3</h3>
+                    <p class="mb-0">Discuss customization needs and receive samples if required</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="steps-order">
+                    <h3>Step 4</h3>
+                    <p class="mb-0">Place your order with specified quantities and customizations</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="steps-order">
+                    <h3>Step 5</h3>
+                    <p class="mb-0">Confirm order details and proceed with the agreed payment terms</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="steps-order">
+                    <h3>Step 6</h3>
+                    <p class="mb-0">Track your order through our advanced logistics system</p>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <div class="bg-image d-flext align-items-stretch flex-direction-column h-100 p-3 rounded-3"
+                    style="background-image:linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('assets/images/cta.jpg')">
+                    <h2 class="text-white">Explore products <br>effortlessly in style</h2>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-space" style="background: var(--theme-color);">
+    <div class="container">
+        <div class="row g-4 ">
+            <div class="col-lg-6 order-lg-1 order-2">
+                <img src="assets/images/quality-assurance.jpg" class="img-fluid rounded">
+            </div>
+            <div class="col-lg-6 order-lg-2 order-1 align-self-center">
+                <h2 class="mb-4">Quality Assurance</h2>
+                <p> All products undergo rigorous quality checks</p>
+                <ul class="mt-4">
+                    <li class="d-inline-block mb-3 me-3 fs-5 fw-medium"><i class="ri-verified-badge-fill me-2"></i> Raw
+                        material testing</li>
+                    <li class="d-inline-block mb-3 fs-5 fw-medium"><i class="ri-verified-badge-fill me-2"></i>
+                        In-process quality control</li>
+                    <li class="d-inline-block mb-3 fs-5 fw-medium "><i class="ri-verified-badge-fill me-2"></i> Finished
+                        product inspection</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-space position-relative">
+    <div class="container">
+        <div class="row g-4 justify-content-center">
+            <div class="col-md-6">
+                <h2 class="mb-5">Customization Process</h2>
+                <img src="assets/images/bed-shape.png" alt="" class="img-fluid w-100">
+            </div>
+            <div class="col-md-6 ps-5">
+                <div class="customize-process">
+                    <div class="steps">
+                        <span class="numb">1</span>
+                        <h5 class="mb-0">Initial consultation to understand your requirements</h5>
+                    </div>
+                    <div class="steps  ms-5">
+                        <span class="numb">2</span>
+                        <h5 class="mb-0">Design and sample creation</h5>
+                    </div>
+                    <div class="steps ">
+                        <span class="numb">3</span>
+                        <h5 class="mb-0">Feedback and refinement</h5>
+                    </div>
+                    <div class="steps  ms-5">
+                        <span class="numb">4</span>
+                        <h5 class="mb-0">Prototype approval</h5>
+                    </div>
+                    <div class="steps ">
+                        <span class="numb">5</span>
+                        <h5 class="mb-0">Bulk production</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="bed-shape"><img src="assets/images/bed-shape.png" alt="" class="img-fluid w-50"></div> -->
+</section>
+<?php include('include/footer.php'); ?>
