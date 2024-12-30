3afefef1f838076c0f5ddb2384d6a71cf7341f6e
Amit Kushwaha <akkushwaha018@gmail.com>
2024-12-30 12:39:36 +0530

    Manufacturing page one section createed

--- manufacturing-capabilities.php
+++ manufacturing-capabilities.php
@@ -33,4 +33,103 @@
     </div>
 </section>
 
+<section class="textile-processing">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h3>
+                    Advanced Textile Processing
+                </h3>
+                <p>
+                    Our manufacturing process encompasses all stages of textile production, adhering to rigorous quality standards throughout:
+                </p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="texttile-tabs">
+                    <ul class="nav nav-pills justify-content-center mb-3" id="pills-tab" role="tablist">
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link img-boxx active" id="pills-spin-tab" data-bs-toggle="pill" data-bs-target="#pills-spin" type="button" role="tab" aria-controls="pills-spin" aria-selected="true">
+                    <img src="assets/images/spinning.png" class="img-fluid mb-3">    Spinning
+                    </button>
+                  </li>
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link img-boxx" id="pills-weav-tab" data-bs-toggle="pill" data-bs-target="#pills-weav" type="button" role="tab" aria-controls="pills-weav" aria-selected="false">
+                        <img src="assets/images/weaving.png" class="img-fluid mb-3"> Weaving
+                    </button>
+                  </li>
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link img-boxx" id="pills-knit-tab" data-bs-toggle="pill" data-bs-target="#pills-knit" type="button" role="tab" aria-controls="pills-knit" aria-selected="false">
+                        <img src="assets/images/knitting.png" class="img-fluid mb-3"> Knitting
+                    </button>
+                  </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                  <div class="tab-pane fade show active" id="pills-spin" role="tabpanel" aria-labelledby="pills-spin-tab">
+                      <div class="row align-items-center mt-4">
+                          <div class="col-lg-6">
+                              <ul class="text-process">
+                                <li>
+                                    Cutting-edge spinning technology for various yarn types
+                                </li>
+                                <li>
+                                    Capacity to produce fine counts up to 120s
+                                </li>
+                                <li>
+                                    Blending capabilities for cotton, polyester, and specialty fibers
+                                </li>
+                              </ul>
+                          </div>
+                          <div class="col-lg-6">
+                              <img src="assets/images/spinning.jpg" class="img-fluid">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="tab-pane fade" id="pills-weav" role="tabpanel" aria-labelledby="pills-weav-tab">
+                      <div class="row align-items-center mt-4">
+                          <div class="col-lg-6">
+                              <ul class="text-process">
+                                  <li>
+                                    High-speed air jet and rapier looms
+                                  </li>
+                                  <li>
+                                    Production of wide-width fabrics up to 120 inches
+                                  </li>
+                                  <li>
+                                    Specialty weaves including jacquard and dobby
+                                  </li>
+                              </ul>
+                          </div>
+                          <div class="col-lg-6">
+                              <img src="assets/images/weaving.jpg" class="img-fluid">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="tab-pane fade" id="pills-knit" role="tabpanel" aria-labelledby="pills-knit-tab">
+                      <div class="row align-items-center mt-4">
+                          <div class="col-lg-6">
+                              <ul class="text-process">
+                                  <li>
+                                      Circular and flat knitting machines
+                                  </li>
+                                  <li>
+                                      Production of single jersey, rib, interlock, and pique fabrics
+                                  </li>
+                                  <li>
+                                      Seamless technology for specialized products
+                                  </li>
+                              </ul>
+                          </div>
+                          <div class="col-lg-6">
+                              <img src="assets/images/knitting.jpg" class="img-fluid">
+                          </div>
+                      </div>
+                  </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include ('include/footer.php'); ?>
